8e1afc29839db9cd85b8007e378b2a5441635186
Tim Terwijn <tim@tterwijn.nl>
2020-06-15 12:39:34 +0200

    submenu pupup werkt

--- components/submenu/submenu.js
+++ components/submenu/submenu.js
@@ -11,6 +11,9 @@ function load(element){
 
         //fix button links
         setLinks();
+
+        //set info button links
+        setInfoButtonsEvent();
     });
 }
 
@@ -37,6 +40,43 @@ function setLinks(){
             const link = "/" + name;
             breadcrumb.insertAdjacentHTML("beforeend",link);
 
+        }, false);
+    }
+}
+
+function setInfoButtonsEvent(){
+    const info = document.querySelector("#submenu>article>section");
+    const infoButtons = document.getElementsByClassName("subinfo-button");
+
+    const descriptions = [
+        "Hier zie je jouw nieuws.",
+        "Hier kan je jouw mail lezen of versturen.",
+        "Hier kan je alles over de school lezen.",
+        "Hier vind je jouw opdrachten en kan je ze inleveren.",
+        "Hier zie je jouw cijfers",
+        "Hier vind je lijsten wat je af moet maken.",
+        "Hier kun je boeken lenen.",
+        "Hier kun je informatie over de opleiding vinden.",
+        "Hier zie je waar je elk HvA gebouw kan vinden.",
+        "Hier zie je dingen over de HvA organisatie.",
+        "Hier kun je instellen waar je elke knop wilt hebben. (Work in progress.)",
+    ];
+
+    let i = 0;
+    for (const infoButton of infoButtons) {
+        const description = descriptions[i++];
+        infoButton.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            //change description of info
+            info.firstElementChild.innerHTML = description;
+            
+            //show info
+            info.classList.remove("hidden");
+
+            //set button to black
+            infoButton.style.color = "black";
+            infoButton.style.textDecoration = "none";
         }, false);
     }
 }
