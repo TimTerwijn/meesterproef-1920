10a2b39b15e1c333966ebeb4b397984a11514d9f
Tim Terwijn <tim@tterwijn.nl>
2020-06-16 09:57:23 +0200

    ie shows a screen now

--- js/oldApp.js
+++ js/oldApp.js
@@ -1,1 +1,46 @@
-alert("ye")
+function init(){
+    loadContainer();
+}
+
+init();
+
+function fetch(file, targetElement){
+    
+}
+
+function loadContainer(){
+    const file = "./components/container/container.html";
+    const targetElement = document.querySelector("body>main");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           targetElement.innerHTML = xhttp.responseText;
+
+           loadMenu();
+        //    loadSubMenu();
+        }
+    };
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+function loadMenu(){
+    const file = "./components/menu/menu.html";
+    const targetElement = document.querySelector("#container>article>section");
+   
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           // Typical action to be performed when the document is ready:
+           targetElement.innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", file, true);
+    xhttp.send();
+}
+
+function loadSubMenu(){
+
+}
