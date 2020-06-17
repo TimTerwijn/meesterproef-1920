ba5a9830b7d9536195340b70ef8601672571c992
Tim Terwijn <tim@tterwijn.nl>
2020-06-17 10:29:35 +0200

    added a swipe functionality

--- components/container/container.js
+++ components/container/container.js
@@ -34,6 +34,25 @@ function loadComponents(){
 
 function setLinks(){
     const parent = document.querySelector("#container>article");
+    const mobileMenu = parent.children[2];
+    const menuButton = mobileMenu.children[0];
+    const subMenuButton = mobileMenu.children[1];
+    
+    menuButton.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        showMenu();
+    });
+
+    subMenuButton.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        showSubMenu();
+    });
+}
+
+function showMenu(){
+    const parent = document.querySelector("#container>article");
     const menu = parent.children[0];
     const submenu = parent.children[1];
     const mobileMenu = parent.children[2];
@@ -41,39 +60,43 @@ function setLinks(){
     const menuButton = mobileMenu.children[0];
     const subMenuButton = mobileMenu.children[1];
 
-    menuButton.addEventListener("click", function(event) {
-        event.preventDefault();
+    //switch icons
+    menuButton.classList.remove("far");
+    menuButton.classList.add("fas");
+    
 
-        //switch icons
-        menuButton.classList.toggle("fas");
-        menuButton.classList.toggle("far");
+    subMenuButton.classList.remove("fas");
+    subMenuButton.classList.add("far");
 
-        subMenuButton.classList.toggle("fas");
-        subMenuButton.classList.toggle("far");
+    //show menu
+    submenu.classList.remove("block");
+    submenu.classList.add("gone");
+    menu.classList.add("block");
+    menu.classList.remove("gone");
+}
 
-        //show menu
-        submenu.classList.remove("block");
-        submenu.classList.add("gone");
-        menu.classList.add("block");
-        menu.classList.remove("gone");
-    });
+function showSubMenu(){
+    const parent = document.querySelector("#container>article");
+    const menu = parent.children[0];
+    const submenu = parent.children[1];
+    const mobileMenu = parent.children[2];
+    
+    const menuButton = mobileMenu.children[0];
+    const subMenuButton = mobileMenu.children[1];
 
-    subMenuButton.addEventListener("click", function(event) {
-        event.preventDefault();
+    //switch icons
+    menuButton.classList.remove("fas");
+    menuButton.classList.add("far");
 
-        //switch icons
-        menuButton.classList.toggle("fas");
-        menuButton.classList.toggle("far");
+    subMenuButton.classList.remove("far");
+    subMenuButton.classList.add("fas");
+    
 
-        subMenuButton.classList.toggle("fas");
-        subMenuButton.classList.toggle("far");
-
-        //show submenu
-        menu.classList.remove("block");
-        menu.classList.add("gone");
-        submenu.classList.add("block");
-        submenu.classList.remove("gone");
-    });
+    //show submenu
+    menu.classList.remove("block");
+    menu.classList.add("gone");
+    submenu.classList.add("block");
+    submenu.classList.remove("gone");
 }
 
 function setServiceWorker(){
@@ -90,6 +113,62 @@ function setServiceWorker(){
         console.log('Service workers are not supported.');
     }
 }
+
+//swipe function
+
+//check if on mobile
+const isMobile = window.matchMedia("only screen and (max-width: 1065px)").matches;
+
+if(isMobile){
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
+}
+
+let xDown = null;                                                        
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches;
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    const isInMenu = document.getElementById("container");
+    
+    if ( ! xDown || ! yDown || isInMenu === null) {
+        return;
+    }
+
+    const xUp = evt.touches[0].clientX;                                    
+    const yUp = evt.touches[0].clientY;
+
+    const xDiff = xDown - xUp;
+    const yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            showSubMenu();
+        } else {
+            /* right swipe */
+            showMenu();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 export {
     load,
